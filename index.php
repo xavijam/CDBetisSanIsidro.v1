d01506a8f93d6d548eb99eafb62876c74a665fce
xavijam <xavijam@gmail.com>
2011-12-12 23:22:59 +0100

    porra modal added

--- index.php
+++ index.php
@@ -28,11 +28,19 @@
 
   <?php get_footer(); ?>
   
-  <div class="modal_partners">
+<!--   <div class="modal_partners">
     <a class="close" href="#cerrar">X</a>
     <h3>Ahora ser socio es más fácil que nunca...</h3>
     <p>En 5 minutos podrás hacerlo, <a href="/socio/">rellena el formulario</a> con<br/>tus datos y en breve pertenecerás a un club de más de 80 años de historia...</p>
     <p><small>Recuerda, si quieres renovar tu subscripción,<br/> indica tu número de socio del año pasado</small></p>
+  </div> -->
+
+    
+	<div class="modal_partners">
+    <a class="close" href="#cerrar">X</a>
+    <h3>Premio porra mes de diciembre</h3>
+    <p>"Os informamos que el premio de la porra del mes de diciembre se entregará con el sorteo del próximo sábado dia 17, debido a que es el último sábado del mes en que se realizará sorteo de la lotería nacional. Gracias por vuestra colaboración"</p>
+    <p><small>Un saludo<br/>CD Betis San Isidro</small></p>
   </div>
 
 
